4670d1c01c27f701d8b423c8e41423df9dd8f690
veeramaninenmenisundaram <nsveeramanisundaram4293@gmail.com>
2020-05-15 13:25:25 +0530

    ChangesforCrossOrigin

--- src/main/java/com/simtuitive/core/Application.java
+++ src/main/java/com/simtuitive/core/Application.java
@@ -2,9 +2,10 @@ package com.simtuitive.core;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-
+@CrossOrigin
 @SpringBootApplication
 public class Application {
 
