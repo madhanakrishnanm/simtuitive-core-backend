6ccff6ac0570f303e2bf6ab310e9e6467ad82e73
veeramaninenmenisundaram <nsveeramanisundaram4293@gmail.com>
2020-05-15 13:39:23 +0530

    CrossOrigin

--- src/main/java/com/simtuitive/core/oauth2/AuthTokenFilter.java
+++ src/main/java/com/simtuitive/core/oauth2/AuthTokenFilter.java
@@ -73,6 +73,12 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 //		}
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse res = (HttpServletResponse)response;
+		res.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		res.setHeader("Access-Control-Max-Age", "3600");
+		res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
 		request=new HttpServletRequestWrapper((HttpServletRequest)request);
 		
 		System.out.println("request after casting"+req.getParameter("username"));
