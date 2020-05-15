424bddaab6ec0f08b5f6f683ad5a1e392494088a
veeramaninenmenisundaram <nsveeramanisundaram4293@gmail.com>
2020-05-15 17:47:50 +0530

    ChangesforOrigin

--- src/main/java/com/simtuitive/core/oauth2/AuthTokenFilter.java
+++ src/main/java/com/simtuitive/core/oauth2/AuthTokenFilter.java
@@ -77,7 +77,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 		res.setHeader("Access-Control-Allow-Credentials", "true");
 		res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		res.setHeader("Access-Control-Max-Age", "3600");
-		res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+		res.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type, Accept, X-Requested-With, remember-me");
 
 		request=new HttpServletRequestWrapper((HttpServletRequest)request);
 		
